dd89387c2d0f64aa58f658811aabd57c989002df
Lucas Botelho <lucas.botelho@isobar.com>
2023-05-16 20:13:58 +0100

    front page

--- dev-portfolio/src/app/page.tsx
+++ dev-portfolio/src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
-import { Flex, Text, Image, Spacer } from "@chakra-ui/react";
+import { Flex, Text, Image, Spacer, Button } from "@chakra-ui/react";
 
 export default function Home() {
+  const style3 = {
+    transform: "rotate(180deg)",
+    color: "#7ac4c2",
+    marginTop: "10px",
+    marginLeft: "15px",
+  };
   return (
     <>
       <Flex
-        direction={"row"}
+        direction={"column"}
         margin={"10px 0px 10px 0px"}
         bgColor={"white"}
         align={"center"}
@@ -15,21 +21,47 @@ export default function Home() {
         borderColor="gray.200"
         borderRadius={"40px"}
       >
-        <Flex marginLeft={"10%"}>
-          <Text fontSize={"5xl"}>Lucas Botelho</Text>
+        <Flex direction={"row"} minW={"100%"} align={"center"}>
+          <Text fontSize={"5xl"} marginLeft={"18vw"}>
+            Lucas Botelho
+          </Text>
+          <Spacer />
+          <Flex padding={"10px 18vw 10px 15px"} align={"revert"}>
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="https://media.licdn.com/dms/image/C4D03AQHTDv00ZE_fFg/profile-displayphoto-shrink_800_800/0/1567180607259?e=1689811200&v=beta&t=o99n3isZKiV1xqgcFtrzZBApcyFwIoF7yzQgfPv-2_M"
+              alt="Dan Abramov"
+            />
+          </Flex>
         </Flex>
-        <Spacer />
-        <Flex
-          padding={"10px 10px 10px 15px"}
-          marginRight={"10%"}
-          align={"revert"}
-        >
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            src="https://bit.ly/dan-abramov"
-            alt="Dan Abramov"
-          />
+
+        <Flex align={"left"} maxW={"75%"} direction={"column"}>
+          <Text fontSize={"xl"} as="b" marginBottom={"10px"} color={"#7ac4c2"}>
+            About me
+          </Text>
+          <Text fontSize={"l"} marginBottom={"10px"}>
+            Hello! I'm a passionate Sitecore software developer with 4 years of
+            professional experience and an ongoing pursuit of a bachelor's
+            degree in software engineering.
+          </Text>
+          <Text fontSize={"l"} marginBottom={"10px"}>
+            Aside from my technical pursuits, I have a deep appreciation for
+            craftsmanship. I find joy in solving mechanical issues on vehicles,
+            which fuels my curiosity and problem-solving skills in diverse
+            domains, allowing me to unleash my creativity while honing my
+            attention to detail.
+          </Text>
+          <Text fontSize={"l"} marginBottom={"10px"}>
+            Another aspect of my life that brings me immense joy is traveling.
+            With passion for adventure and exploration, I have broadened my
+            horizons and instilled a sense of adaptability.
+          </Text>
+          <Text fontSize={"l"} marginBottom={"10px"}>
+            I believe that the combination of my technical expertise,
+            problem-solving mindset, and passion for exploration allows me to
+            approach software development with an exciting perspective.
+          </Text>
         </Flex>
       </Flex>
     </>
--- dev-portfolio/src/components/Navbar/Navbar.tsx
+++ dev-portfolio/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import { ArrowBackIcon } from "@chakra-ui/icons";
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import React from "react";
 import { useRouter } from "next/navigation";
 import RightContent from "./RightContent/RightContent";
-
+import { TiSocialLinkedin } from "react-icons/ti";
+import { SiGithub } from "react-icons/si";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/react";
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
@@ -11,7 +14,7 @@ const Navbar: React.FC<NavbarProps> = () => {
 
   return (
     <Flex bg={"white"} height={"44px"} padding={"6px 12px"} align={"center"}>
-      <Flex align={"center"} onClick={() => router.back()} flexGrow={1}>
+      <Flex align={"center"} onClick={() => router.back()}>
         <ArrowBackIcon
           _hover={{ color: "#7ac4dd" }}
           color={"gray.400"}
@@ -19,6 +22,39 @@ const Navbar: React.FC<NavbarProps> = () => {
           w={"35px"}
           mr={2}
         />
+      </Flex>
+      <Flex align={"center"}>
+        <Link
+          as={NextLink}
+          href={"https://www.linkedin.com/in/lbotelho17/"}
+          isExternal
+        >
+          <Button variant={"solid"}>
+            <TiSocialLinkedin style={{ height: "25px", width: "30px" }} />
+          </Button>
+        </Link>
+      </Flex>
+      <Flex align={"center"}>
+        <Link
+          as={NextLink}
+          href={"public\3a641126-51be-4d97-8a00-5f905a54ad0a.jpg"}
+          isExternal
+        >
+          <Button variant={"solid"}>
+            <TiSocialLinkedin style={{ height: "25px", width: "30px" }} />
+          </Button>
+        </Link>
+      </Flex>
+      <Flex align={"center"} flexGrow={1}>
+        <Link
+          as={NextLink}
+          href={"https://www.linkedin.com/in/lbotelho17/"}
+          isExternal
+        >
+          <Button variant={"solid"}>
+            <SiGithub style={{ height: "25px", width: "30px" }} />
+          </Button>
+        </Link>
       </Flex>
       <RightContent />
     </Flex>
